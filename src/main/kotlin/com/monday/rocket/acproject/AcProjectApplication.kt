8f0306882873fdee843021092eefc2e634e79600
monica <monica@daangn.com>
2022-11-14 16:53:39 +0900

    feat: set up project

---
+++ src/main/kotlin/com/monday/rocket/acproject/AcProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.monday.rocket.acproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AcProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<AcProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/monday/rocket/acproject/AcProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.monday.rocket.acproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AcProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
